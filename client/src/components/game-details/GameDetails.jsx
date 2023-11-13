7b3dc579f5c42800945d639266110b8440df8a97
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-11-13 21:57:59 +0200

    Add new comment to local state

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -28,7 +28,7 @@ export default function GameDetails() {
             formData.get('comment')
         );
 
-        console.log(newComment);
+        setComments(state => [...state, newComment]);
     }
 
     return (
@@ -47,8 +47,8 @@ export default function GameDetails() {
                 <div className="details-comments">
                     <h2>Comments:</h2>
                     <ul>
-                        {comments.map(({ username, text }) => (
-                            <li className="comment">
+                        {comments.map(({ _id, username, text }) => (
+                            <li key={_id} className="comment">
                                 <p>{username}: {text}</p>
                             </li>
                         ))}
