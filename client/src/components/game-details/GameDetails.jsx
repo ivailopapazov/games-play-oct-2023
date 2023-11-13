439462dcb0d5b7df65038f2968ba8cb0214a2d0c
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-11-13 21:53:29 +0200

    Save comments

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -6,11 +6,15 @@ import * as commentService from '../../services/commentService';
 
 export default function GameDetails() {
     const [game, setGame] = useState({});
+    const [comments, setComments] = useState([]);
     const { gameId } = useParams();
 
     useEffect(() => {
         gameService.getOne(gameId)
             .then(setGame);
+
+        commentService.getAll()
+            .then(setComments);
     }, [gameId]);
 
     const addCommentHandler = async (e) => {
@@ -43,14 +47,16 @@ export default function GameDetails() {
                 <div className="details-comments">
                     <h2>Comments:</h2>
                     <ul>
-                        <li className="comment">
-                            <p>Content: I rate this one quite highly.</p>
-                        </li>
-                        <li className="comment">
-                            <p>Content: The best game.</p>
-                        </li>
+                        {comments.map(({ username, text }) => (
+                            <li className="comment">
+                                <p>{username}: {text}</p>
+                            </li>
+                        ))}
                     </ul>
-                    <p className="no-comment">No comments.</p>
+
+                    {comments.length === 0 && (
+                        <p className="no-comment">No comments.</p>
+                    )}
                 </div>
 
                 {/* <!-- Edit/Delete buttons ( Only for creator of this game )  -->
--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -2,6 +2,12 @@ import * as request from '../lib/request';
 
 const baseUrl = 'http://localhost:3030/jsonstore/comments';
 
+export const getAll = async () => {
+    const result = await request.get(baseUrl);
+
+    return Object.values(result); 
+};
+
 export const create = async (gameId, username, text) => {
     const newComment = await request.post(baseUrl, {
         gameId,
@@ -10,4 +16,4 @@ export const create = async (gameId, username, text) => {
     });
 
     return newComment;
-}
+};
