049c94d1e97822dd0a29850fb6af68cc24825f41
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-11-13 19:40:53 +0200

    Add game create page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import GameList from './components/game-list/GameList';
+import GameCreate from './components/game-create/GameCreate';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/games" element={<GameList />} />
+                <Route path="/games/create" element={<GameCreate />} />
             </Routes>
         </div>
     )
---
+++ client/src/components/game-create/GameCreate.jsx
@@ -0,0 +1,27 @@
+export default function GameCreate() {
+    return (
+        <section id="create-page" className="auth">
+            <form id="create">
+                <div className="container">
+
+                    <h1>Create Game</h1>
+                    <label htmlFor="leg-title">Legendary title:</label>
+                    <input type="text" id="title" name="title" placeholder="Enter game title..." />
+
+                    <label htmlFor="category">Category:</label>
+                    <input type="text" id="category" name="category" placeholder="Enter game category..." />
+
+                    <label htmlFor="levels">MaxLevel:</label>
+                    <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" />
+
+                    <label htmlFor="game-img">Image:</label>
+                    <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
+
+                    <label htmlFor="summary">Summary:</label>
+                    <textarea name="summary" id="summary"></textarea>
+                    <input className="btn submit" type="submit" value="Create Game" />
+                </div>
+            </form>
+        </section>
+    );
+}
