b624ec2b960338ce93c936772d24a5bcea727bde
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-11-13 19:43:42 +0200

    Add register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/home/Home"
 import GameList from './components/game-list/GameList';
 import GameCreate from './components/game-create/GameCreate';
 import Login from './components/login/Login';
+import Register from './components/register/Register';
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/games" element={<GameList />} />
                 <Route path="/games/create" element={<GameCreate />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </div>
     )
---
+++ client/src/components/register/Register.jsx
@@ -0,0 +1,27 @@
+export default function Register() {
+    return (
+        <section id="register-page" className="content auth">
+            <form id="register">
+                <div className="container">
+                    <div className="brand-logo"></div>
+                    <h1>Register</h1>
+
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="maria@email.com" />
+
+                    <label htmlFor="pass">Password:</label>
+                    <input type="password" name="password" id="register-password" />
+
+                    <label htmlFor="con-pass">Confirm Password:</label>
+                    <input type="password" name="confirm-password" id="confirm-password" />
+
+                    <input className="btn submit" type="submit" value="Register" />
+
+                    <p className="field">
+                        <span>If you already have profile click <a href="#">here</a></span>
+                    </p>
+                </div>
+            </form>
+        </section>
+    );
+}
