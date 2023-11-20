86c5644ac01b01faba447c7e38bae25fb5b927fc
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-11-20 18:42:13 +0200

    Change login to controlled form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,16 +1,36 @@
+import useForm from "../../hooks/useForm";
+
 export default function Login() {
+    const { values, onChange, onSubmit } = useForm({
+        email: '',
+        password: '',
+    });
+
     return (
         <section id="login-page" className="auth">
-            <form id="login">
+            <form id="login" onSubmit={onSubmit}>
 
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Sokka@gmail.com"
+                        onChange={onChange}
+                        value={values.email}
+                    />
 
                     <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
+                    <input
+                        type="password"
+                        id="login-password"
+                        name="password"
+                        onChange={onChange}
+                        value={values.password}
+                    />
                     <input type="submit" className="btn submit" value="Login" />
                     <p className="field">
                         <span>If you don't have profile click <a href="#">here</a></span>
