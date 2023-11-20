4cdc061aa345bad26eaa6eaa38f4963b77bbd8a2
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-11-20 21:44:03 +0200

    Migrate to collections from jsonstore

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,11 +1,11 @@
 import * as request from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/jsonstore/games'
+const baseUrl = 'http://localhost:3030/data/games'
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
 
-    return Object.values(result);
+    return result;
 };
 
 export const getOne = async (gameId) => {
